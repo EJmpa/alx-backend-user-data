4f07e6330bccc683e63997c26f1f086cb04202b1
EJmpa <chukwuemeka.chukwu@eng.uniben.edu>
2023-11-03 01:18:44 +0000

    logging

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Module for filtering datum
+"""
+import re
+from typing import List
+
+
+def filter_datum(
+    fields: List[str], redaction: str, message: str, separator: str
+) -> str:
+    """
+    Returns the log message obfuscated
+    """
+    for field in fields:
+        message = re.sub(f"{field}=.+?{separator}",
+                         f"{field}={redaction}{separator}", message)
+    return message
