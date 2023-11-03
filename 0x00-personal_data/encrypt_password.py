e4d3fa505e74f7d2df827e617a0426afbd9de693
EJmpa <chukwuemeka.chukwu@eng.uniben.edu>
2023-11-03 09:25:49 +0000

    personal_data

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -10,3 +10,11 @@ def hash_password(password: str) -> bytes:
     Returns a salted, hashed password, which is a byte string.
     """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    Returns True if the provided password matches the hashed
+    password, False otherwise.
+    """
+    return bcrypt.checkpw(password.encode(), hashed_password)
