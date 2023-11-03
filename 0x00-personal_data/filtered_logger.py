05fe35bfcc243c6a1ce3f2ea63a9e7f443a27f8f
EJmpa <chukwuemeka.chukwu@eng.uniben.edu>
2023-11-03 01:51:33 +0000

    logging;git push git add .;git commit -m logging;git push
    git add .;git commit -m logging

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,6 +3,7 @@
 Module for filtering datum
 """
 import re
+import logging
 from typing import List
 
 
@@ -16,3 +17,25 @@ def filter_datum(
         message = re.sub(f"{field}=.+?{separator}",
                          f"{field}={redaction}{separator}", message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """
+        Returns the log message obfuscated
+        """
+        message = super().format(record)
+        return filter_datum(
+            self.fields, self.REDACTION, message, self.SEPARATOR
+        )
