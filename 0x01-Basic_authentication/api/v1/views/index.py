3171364d5a55d9ab6dc811e4a5150e68cb99c594
Chukwuemeka Jude Chukwu <chukwuemeka.chukwu@eng.uniben.edu>
2023-11-10 02:03:54 +0100

    Update index.py

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -30,3 +30,11 @@ def stats() -> str:
 def unauthorized_endpoint() -> None:
     """Raise a 401 error."""
     abort(401)
+
+
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden() -> str:
+    """ GET /api/v1/unauthorized
+      - raises 403 error
+    """
+    abort(403)
