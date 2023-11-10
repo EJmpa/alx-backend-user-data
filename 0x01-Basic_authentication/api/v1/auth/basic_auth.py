5155e82d974f03fd8f855707b2823e570c14bd07
EJmpa <chukwuemeka.chukwu@eng.uniben.edu>
2023-11-10 04:09:33 +0000

    Basic Auth

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,6 +4,9 @@ Module containing BasicAuth class for authentication.
 """
 
 import base64
+from typing import TypeVar
+from models.user import User
+from models.base import DATA
 from api.v1.auth.auth import Auth
 
 
@@ -83,3 +86,30 @@ class BasicAuth(Auth):
         email, pswd = decoded_base64_authorization_header.split(':', 1)
 
         return email, pswd
+
+    def user_object_from_credentials(
+            self, user_email: str, user_pwd: str) -> TypeVar('User'):
+        """
+        Method to get the User instance based on email and password.
+
+        Args:
+            user_email (str): The user email.
+            user_pwd (str): The user password.
+
+        Returns:
+            TypeVar('User'): The User instance if found, else None.
+        """
+        if user_email is None or not isinstance(user_email, str):
+            return None
+
+        if user_pwd is None or not isinstance(user_pwd, str):
+            return None
+
+        try:
+            users = User.search({'email': user_email})
+        except Exception:
+            return None
+        for user in users:
+            if user.is_valid_password(user_pwd):
+                return user
+        return None
