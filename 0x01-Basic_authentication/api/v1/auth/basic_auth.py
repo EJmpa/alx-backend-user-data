582029e35c14ebc1ee7679934320ea58a2f35cc0
EJmpa <chukwuemeka.chukwu@eng.uniben.edu>
2023-11-10 03:06:57 +0000

    Basic Auth

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,7 @@
 Module containing BasicAuth class for authentication.
 """
 
+import base64
 from api.v1.auth.auth import Auth
 
 
@@ -10,4 +11,28 @@ class BasicAuth(Auth):
     """
     BasicAuth class inherits from Auth.
     """
-    pass
+
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
+        """
+        Method to extract the Base64 part of the Authorization header for
+        Basic Authentication.
+
+        Args:
+            authorization_header (str): The Authorization header string.
+
+        Returns:
+            str: The Base64 part of the Authorization header,
+            or None if not valid.
+        """
+        if authorization_header is None or \
+                not isinstance(authorization_header, str):
+            return None
+
+        if not authorization_header.startswith("Basic "):
+            return None
+
+        # Extract the Base64 part after "Basic "
+        base64_part = authorization_header.split("Basic ")[1]
+
+        return base64_part if base64_part else None
