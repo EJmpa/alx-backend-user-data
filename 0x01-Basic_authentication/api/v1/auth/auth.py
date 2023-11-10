393f526175dfcd1c3e894392b8c660fa631d7901
Chukwuemeka Jude Chukwu <chukwuemeka.chukwu@eng.uniben.edu>
2023-11-10 03:09:31 +0100

    Update auth.py

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -43,9 +43,9 @@ class Auth:
         Returns:
             str: The authorization header or None if not present.
         """
-        if request:
-            return request.headers.get('Authorization')
-        return None
+        if request is None or 'Authorization' not in request.headers:
+            return None
+        return request.headers.get('Authorization')
 
     def current_user(self, request=None) -> TypeVar('User'):
         """
