4bed8864066ff47835193f7f69eda77ba1562d6a
EJmpa <chukwuemeka.chukwu@eng.uniben.edu>
2023-11-10 02:47:25 +0000

    Basic Auth

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Module containing BasicAuth class for authentication.
+"""
+
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """
+    BasicAuth class inherits from Auth.
+    """
+    pass
+
