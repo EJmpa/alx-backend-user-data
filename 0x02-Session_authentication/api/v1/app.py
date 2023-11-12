3b85ef86afe78509b58a4fcd9e2e7e7cd874f9ce
Chukwuemeka Jude Chukwu <chukwuemeka.chukwu@eng.uniben.edu>
2023-11-13 00:20:51 +0100

    Update

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -77,8 +77,6 @@ def before_request_func():
     if auth.authorization_header(request) is None and \
             auth.session_cookie(request) is None:
         abort(401)
-    if auth.authorization_header(request) is None:
-        abort(401)
     if auth.current_user(request) is None:
         abort(403)
     request.current_user = auth.current_user(request)
