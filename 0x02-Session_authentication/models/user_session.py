ed59e2386ea60b09def99af544791dd91632a3b3
Chukwuemeka Jude Chukwu <chukwuemeka.chukwu@eng.uniben.edu>
2023-11-13 02:12:45 +0100

    Update user_session.py

--- 0x02-Session_authentication/models/user_session.py
+++ 0x02-Session_authentication/models/user_session.py
@@ -1,18 +1,22 @@
 #!/usr/bin/env python3
 """
-Module for UserSession
+UserSession model module
 """
+from sqlalchemy import Column, String, ForeignKey
 from models.base import Base
-
 
 class UserSession(Base):
     """
     UserSession class
     """
+    __tablename__ = 'user_sessions'
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    session_id = Column(String(60), nullable=False)
+
     def __init__(self, *args: list, **kwargs: dict):
         """
-        Initialize UserSession
+        Initialize UserSession instance
         """
         super().__init__(*args, **kwargs)
-        self.user_id = kwargs.get('user_id')
-        self.session_id = kwargs.get('session_id')
+        self.user_id = kwargs.get('user_id', '')
+        self.session_id = kwargs.get('session_id', '')
